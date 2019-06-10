7d04b12806b78af349afcfffb2072737467c22ca
Maurice Reigman <m_c.r@hotmail.com>
2019-06-10 15:27:10 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,28 +11,28 @@ import {HttpClient} from '@angular/common/http';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy{
-	// assignment 1 solution
+	/*assignment 1 solution*/
 	public readonly myProperty$ = of('hello observable');
 	public readonly myModifiedProperty$ = this.myProperty$.pipe(
 		map(myProperty => `modified! ${myProperty}`)
 	);
 
-	// assignment 2 solution
+	/*assignment 2 solution*/
 	public readonly mySideEffect$ = of('my side effect').pipe(tap(console.log));
 	public readonly cold$ = of('its chilling out here').pipe(tap(console.log));
 	public readonly hot$ = of('summerTime')
 		.pipe(share(), tap(console.log));
 	public readonly mySubject = new BehaviorSubject<string | undefined>(undefined);
 
-	// assignment 3 here
+	/*assignment 3 here*/
 	public readonly show$ = this.showById$(1);
-	// public readonly showCastMembers$ = assignment 3.1 code here
+	// public readonly showCastMembers$ = assignment 4.1 code here
 
 	public readonly shows$ = this.showsBySearch$('green');
-	// public readonly showsCastMembers$ = assignment 3.2 code here
+	// public readonly showsCastMembers$ = assignment 4.2 code here
 
 	public readonly castMembers$ = this.castMembersForShow$(1)
-		.pipe(tap(val => console.log('logging assignment 3.3')));
+		.pipe(tap(val => console.log('logging assignment 3.4')));
 
 	private readonly subscriptions = new Subscription();
 
@@ -41,11 +41,11 @@ export class AppComponent implements OnInit, OnDestroy{
 	}
 
 	public ngOnInit(): void {
-		// assignment 3.3 code here
+		// assignment 4.4 code here
 	}
 
 	public ngOnDestroy(): void {
-		// assignment 3.3 code here
+		// assignment 4.4 code here
 	}
 
 	private showById$(id: number): Observable<Show> {
