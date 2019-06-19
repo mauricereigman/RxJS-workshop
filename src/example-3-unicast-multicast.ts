b36539524c6e4139893f3e2f7feaaf77df11c78b
Maurice Reigman <m_c.r@hotmail.com>
2019-06-19 21:51:20 +0200

    fix

---
+++ src/example-3-unicast-multicast.ts
@@ -0,0 +1,12 @@
+import {Observable} from 'rxjs';
+
+/*const myUniCastObservable = new Observable(observer => {
+	observer.next('my value')
+});*/
+
+/*
+const myValue = 'myValue';
+const myMultiCastObservable = new Observable(observer => {
+	observer.next(myValue);
+});
+*/
