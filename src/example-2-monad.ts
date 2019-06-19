1bd4b6ba3aed03e5081b4ea92c5ba3723c1c9a30
Maurice Reigman <m_c.r@hotmail.com>
2019-06-19 21:06:08 +0200

    added case

--- src/example-2-monad.ts
+++ src/example-2-monad.ts
@@ -8,11 +8,11 @@ console.log(myNumbersTimesTwo);
 */
 
 
-/*
+/* DEMO IN CHROME OR FIREFOX
+
 const myNumbers = [1,2,3,4,5];
 const myMultipliers = [2,3,4];
 
 const myNumbersTimesMyMultipliers = myNumbers.flatMap(number => myMultipliers.map(multiplier => number * multiplier));
 
-console.log(myNumbersTimesTwo);
-*/
+console.log(myNumbersTimesMyMultipliers);*/
